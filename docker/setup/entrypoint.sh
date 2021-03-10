645ef8dfe11005c4294c5a8ceb0f1dfed1bb32dd
Josh Stoik <josh@blobfolio.com>
2021-03-10 15:05:31 -0800

    misc: add volumes for Rust repository/toolchain to reduce network load

--- docker/setup/entrypoint.sh
+++ docker/setup/entrypoint.sh
@@ -4,6 +4,9 @@ set -e
 # Make sure we're inside a container.
 [ -f "/opt/righteous-sandbox.version" ] || exit 1
 
+# Make sure Rust stable is populated.
+[ -d "/usr/local/rustup/toolchains/stable-x86_64-unknown-linux-gnu" ] || mv "/usr/local/rustup/.stable" "/usr/local/rustup/toolchains/stable-x86_64-unknown-linux-gnu"
+
 # Print a fun ASCII greeting.
 echo "                              __"
 echo "                     /\\    .-\" /"
@@ -25,10 +28,11 @@ echo ""
 
 # Parse project settings, if they exist.
 if [ -f "/share/.righteous-sandbox.json" ]; then
-	_apt_update="$( cat "/share/.righteous-sandbox.json" | jq -r '.apt_update' | grep -v "none" )"
-	_npm_install="$( cat "/share/.righteous-sandbox.json" | jq -r '.npm_install' | grep -v "none" )"
-	_just_init="$( cat "/share/.righteous-sandbox.json" | jq -r '.just_init' | grep -v "none" )"
-	_just_list="$( cat "/share/.righteous-sandbox.json" | jq -r '.just_list' | grep -v "none" )"
+	_apt_update="$( cat "/share/.righteous-sandbox.json" | jq -r '.apt_update' | grep -v 'none' )"
+	_npm_install="$( cat "/share/.righteous-sandbox.json" | jq -r '.npm_install' | grep -v 'none' )"
+	_rust_update="$( cat "/share/.righteous-sandbox.json" | jq -r '.rust_update' | grep -v 'none' )"
+	_just_init="$( cat "/share/.righteous-sandbox.json" | jq -r '.just_init' | grep -v 'none' )"
+	_just_list="$( cat "/share/.righteous-sandbox.json" | jq -r '.just_list' | grep -v 'none' )"
 
 	# Run updates?
 	if [ "true" == "${_apt_update,,}" ]; then
@@ -42,10 +46,20 @@ if [ -f "/share/.righteous-sandbox.json" ]; then
 		chown -R --reference /share/package.json /share/node_modules
 	fi
 
+	# Update Rust.
+	if [ "true" == "${_rust_update,,}" ]; then
+		rustup --quiet update
+
+		if [ -f "/share/Cargo.toml" ]; then
+			[ ! -f "/share/Cargo.lock" ] || rm "/share/Cargo.lock"
+			cd /share && cargo update && cargo outdated
+		fi
+	fi
+
 	# Only look at Just-related tasks if there's a justfile present.
 	if [ -f "/share/justfile" ]; then
 		# Initialization with a Just task?
-		if [ ! -z "${_just_init}" ]; then
+		if [ ! -z "${_just_init}" ] && [ "null" != "${_just_init,,}" ]; then
 			cd /share && just "${_just_init}"
 		fi
 
