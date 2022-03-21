afffac57954f9c3af8c0cd506fcef97fb378d487
Josh Stoik <josh@blobfolio.com>
2022-03-20 20:24:21 -0700

    misc: move entrypoint tasks back to bash

--- docker/setup/entrypoint.sh
+++ docker/setup/entrypoint.sh
@@ -26,8 +26,71 @@ echo -e "     '\"\"--.._:               \e[2m$( cat /etc/os-release | grep PRETT
 echo ""
 echo ""
 
+jqval() {
+	cat "/share/.righteous-sandbox.json" | \
+		jq ".$1" | \
+		grep -v null | \
+		xargs echo -n
+}
+
+boolval() {
+	local VAL="$( jqval $1 )"
+	if [ "$VAL" == "true" ] || [ "$VAL" == "true" ]; then
+		echo "true"
+	else
+		echo ""
+	fi
+}
+
 # Parse project settings, if they exist.
-php /opt/entrypoint.php
+if [ -f "/share/.righteous-sandbox.json" ]; then
+	cd /share
+
+	# Apt updates.
+	APT="$( boolval "apt_update" )"
+	if [ "$APT" == "true" ]; then
+		DEBIAN_FRONTEND=noninteractive apt-get update -qq
+		DEBIAN_FRONTEND=noninteractive apt-fast dist-upgrade -y
+	fi
+
+	# Node updates.
+	NPM="$( boolval "npm_install" )"
+	if [ "$NPM" == "true" ]; then
+		if [ -f "/share/package.json" ]; then
+			npm i --quiet
+			chown -R --reference "/share/package.json" "/share/node_modules"
+		fi
+	fi
+
+	# Get rid of any Cargo.lock files.
+	find . -name 'Cargo.lock' -type f -delete
+
+	# Run Just init task?
+	INIT="$( jqval "just_init" )"
+	if [ -n "$INIT" ]; then
+		if [ -f "/share/justfile" ]; then
+			just "$INIT"
+		fi
+	fi
+
+	# Update Rust?
+	RUST="$( boolval "rust_update" )"
+	if [ "$RUST" == "true" ]; then
+		env RUSTUP_PERMIT_COPY_RENAME=true rustup --quiet update 2>/dev/null
+
+		if [ -f "/share/Cargo.toml" ]; then
+			echo ""
+			cargo outdated || true
+			echo ""
+		fi
+	fi
+
+	# List Just tasks?
+	LIST="$( boolval "just_list" )"
+	if [ "$LIST" == "true" ]; then
+		just --list
+	fi
+fi
 
 # Drop to bash.
 exec "/bin/bash"
